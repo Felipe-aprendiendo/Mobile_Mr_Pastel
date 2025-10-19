11104d6c37f600e56512d46ce5d867d8708b469f
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-10-18 22:32:10 -0300

    Crea SplashScreen en ui.screens.splash

---
+++ app/src/main/java/com/grupo3/misterpastel/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,53 @@
+package com.grupo3.misterpastel.ui.screens.splash
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.grupo3.misterpastel.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    // Simula el tiempo de carga de la app (3 segundos)
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate("home") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+
+    // Diseño de la Splash Screen
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_oscuro),
+                contentDescription = "Logo Pastelería 1000 Sabores",
+                modifier = Modifier
+                    .size(300.dp)
+                    .padding(bottom = 16.dp)
+            )
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.tertiary,
+                strokeWidth = 6.dp,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    }
+}
+
