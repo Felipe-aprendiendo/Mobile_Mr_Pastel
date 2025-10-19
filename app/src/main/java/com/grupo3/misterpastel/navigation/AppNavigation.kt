898910635ab976fbcc92cf36e126fcc271967c99
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-10-18 22:21:54 -0300

    Crea AppNavigation para manejo de navController en navigation package

---
+++ app/src/main/java/com/grupo3/misterpastel/navigation/AppNavigation.kt
@@ -0,0 +1,32 @@
+package com.grupo3.misterpastel.navigation
+
+
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AppNavigation(){
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+        //composable ("splash") { SplashScreen(navController) }
+        //composable("home") { HomeScreen(navController) }
+        //composable("login") { LoginScreen(navController) }
+        //composable("registro") { RegistroScreen(navController) }
+        //composable("home_iniciada") { HomeSesionIniciada(navController) }
+        //composable("catalogo") { /* TODO: pendiente again */ }
+    }
+
+
+}
+
