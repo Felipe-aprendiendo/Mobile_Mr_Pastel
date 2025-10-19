70d58663ea33b8d9e88f55e51ab56cd940763215
Omar F.M. <om.felipe@duocuc.cl>
2025-10-19 12:31:10 -0300

    Subiendo Data class Pedido. Manejo de Items mediante una lista y tambien maneja el Estado del pedido

---
+++ app/src/main/java/com/grupo3/misterpastel/model/Pedido.kt
@@ -0,0 +1,10 @@
+package com.grupo3.misterpastel.model
+
+data class Pedido(
+    val id: String,
+    val userId: String,
+    val fecha: Long,
+    val items: List<com.grupo3.misterpastel.repository.CarritoItem>,
+    val total: Double,
+    val estado: com.grupo3.misterpastel.repository.EstadoPedido
+)
