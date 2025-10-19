e84e6d527545a0b0542f361197f836b00619b974
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-10-18 23:40:01 -0300

    Crea DetalleProductoScreen para ver desde catálogo y modifica AppNavigator

--- app/src/main/java/com/grupo3/misterpastel/navigation/AppNavigation.kt
+++ app/src/main/java/com/grupo3/misterpastel/navigation/AppNavigation.kt
@@ -8,11 +8,15 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.grupo3.misterpastel.model.Producto
+import com.grupo3.misterpastel.ui.screens.DetalleProductoScreen
 import com.grupo3.misterpastel.ui.screens.HomeScreen
 import com.grupo3.misterpastel.ui.screens.HomeSesionIniciada
 import com.grupo3.misterpastel.ui.screens.LoginScreen
 import com.grupo3.misterpastel.ui.screens.RegistroScreen
 import com.grupo3.misterpastel.ui.screens.splash.SplashScreen
+import com.grupo3.misterpastel.R
+
 
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -30,9 +34,28 @@ fun AppNavigation(){
         composable("login") { LoginScreen(navController) }
         composable("registro") { RegistroScreen(navController) }
         composable("home_iniciada") { HomeSesionIniciada(navController) }
-        //composable("catalogo") { /* TODO: pendiente again */ }
+        composable(
+            route = "detalle/{productoId}"
+        ) { backStackEntry ->
+            val productoId = backStackEntry.arguments?.getString("productoId")?.toInt() ?: 0
+
+            // TODO: En el futuro, aquí se debe obtener el producto desde el ViewModel:
+            // val producto = viewModel.obtenerProductoPorId(productoId)
+            // Por ahora se genera un producto, pero esto debe desaparecer
+            val producto = Producto(
+                id = productoId,
+                nombre = "Producto de prueba",
+                precio = "$10.000 CLP",
+                imagen = R.drawable.torta_chocolate,
+                categoria = com.grupo3.misterpastel.model.Categoria.TORTA_CUADRADA,
+                descripcion = "Descripción de ejemplo."
+            )
+            DetalleProductoScreen(navController, producto)
+        }
+
+
+
+
     }
-
-
 }
 
