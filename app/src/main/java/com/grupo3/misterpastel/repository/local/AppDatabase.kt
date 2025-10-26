c3f211a551595e3246f2cd433b101ae5f3b3882b
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-10-26 19:33:49 -0300

    Crea AppDatabase, base de datos central de la app

--- app/src/main/java/com/grupo3/misterpastel/repository/local/AppDatabase.kt
+++ app/src/main/java/com/grupo3/misterpastel/repository/local/AppDatabase.kt
@@ -1,2 +1,39 @@
 package com.grupo3.misterpastel.repository.local
 
+
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Base de datos central de la app.
+ * Agrega aquí más entidades si luego usas Room para usuarios, carrito, etc.
+ */
+@Database(
+    entities = [PedidoEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun pedidoDao(): PedidoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "mrpastel_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
